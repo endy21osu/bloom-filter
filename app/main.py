1f9ac4b44b59e86a55dd21669e2ce79e4f8e10e2
jgordon <endy21osu@gmail.com>
2021-05-27 16:29:16 -0400

    set up a large random array  of string, turn bloom filter into a class

--- app/main.py
+++ app/main.py
@@ -2,21 +2,23 @@ from BloomFilter import *
 
 print('welcome to Bloom filter arrays')
 
-print('The test array is: ')
-print(data_array)
+bloom_filter_size = input("what size bloom filter do you want? ")
 
-print('The bloom filter size is: ' + str(bloom_filter_size))
+bloom_filter = BloomFilter(bloom_filter_size)
 
-# bloom_filter_size = input("what size bloom filter do you want? ")
+print('The new bloom filter size is: ' + str(bloom_filter.size))
 #
 # hash_number = input("How many hash's do you want to use? (1-3) ")
 
-print('The False Positive rate limit is set to: %' + str(get_limit()))
+print('The False Positive rate limit is set to: %' + str(bloom_filter.get_limit()))
 
-bloom_filter = build_bloom_filter(data_array)
+bloom_filter.build_bloom_filter(bloom_filter.large_strings_array)
 
-print('The Bloom filter is: ' + bin(bloom_filter))
-print('The False Positive Rate  is: %' + str(get_fp_rate()))
+print('The test array is: ')
+print(bloom_filter.large_strings_array)
+
+print('The Bloom filter is: ' + bin(bloom_filter.bloom_filter_bit_array))
+print('The False Positive Rate  is: %' + str(bloom_filter.get_fp_rate(float(bloom_filter.size))))
 print('')
 additions = 1
 
@@ -25,21 +27,21 @@ while True:
     print('Entry ' + str(additions))
     print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
     new_name = input('What would you like to add to the data set? ')
-    is_in_set = check_name_in_bloom_filter(bloom_filter, new_name)
+    is_in_set = bloom_filter.check_name_in_bloom_filter(bloom_filter.bloom_filter_bit_array, new_name)
 
     if is_in_set:
         print('Sorry the name exists. Please choose a different name')
     else:
         print('Name is not in set. You can use it')
-        bloom_filter = add_name_to_bloom_filter(bloom_filter, new_name)
-        data_array.append(new_name)
+        bloom_filter.add_name_to_bloom_filter(bloom_filter.bloom_filter_bit_array, new_name)
+        bloom_filter.add_to_array(new_name)
         print('The new data set is array is: ')
-        print(data_array)
+        print(bloom_filter.large_strings_array)
         additions += 1
-        print('The New Bloom Filter is: ' + bin(bloom_filter))
-        print('The New False Positive Rate  is: %' + str(get_fp_rate()))
+        print('The New Bloom Filter is: ' + bin(bloom_filter.bloom_filter_bit_array))
+        print('The New False Positive Rate  is: %' + str(bloom_filter.get_fp_rate(bloom_filter.size)))
 
-    if get_fp_rate() > fp_limit:
+    if bloom_filter.get_fp_rate(bloom_filter.size) > bloom_filter.fp_limit:
         print('The False Positive Rate is too high. Please consider adding')
         print('a larger Bloom filter or more hash functions.')
 
